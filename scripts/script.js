6aaaecc38187ac7f7c277f58e7678b7aebc90fe7
Felipe Massashi <Felipe Massashi>
2022-04-21 15:43:27 -0300

    Arquivos

--- scripts/script.js
+++ scripts/script.js
@@ -10,15 +10,17 @@ const getStyle = (element, style) =>
 const initialColors = {
     bg: getStyle(html, "--bg"),
     bgPanel: getStyle(html, "--bg-panel"),
+    bgPrincipal: getStyle(html, "--bg-principal"),
     colorHeadings: getStyle(html, "--color-headings"),
     colorText: getStyle(html, "--color-text"),
 }
 
 const darkMode = {
-    bg: "#31332E",
+    bg: "#171C26",
     bgPanel: "black",
-    colorHeadings: "black",
-    colorText: "#58296b"
+    bgPrincipal: "#293040",
+    colorHeadings: "#5E6D8C",
+    colorText: "rgb(150, 100, 179)"
 }
 
 const transformKey = key => 
