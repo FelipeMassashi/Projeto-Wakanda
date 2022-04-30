5c59883ca768f41d6cce4fc230034bfeac50015e
Felipe Massashi <Felipe Massashi>
2022-04-29 21:31:43 -0300

    Arquivos

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,9 @@
-const html = document.querySelector("html")
-const checkbox = document.querySelector("input[name=theme]")
+const html = document.querySelector("html");
 
 const getStyle = (element, style) => 
     window
         .getComputedStyle(element)
-        .getPropertyValue(style)
+        .getPropertyValue(style);
 
 
 const initialColors = {
@@ -24,16 +23,27 @@ const darkMode = {
 }
 
 const transformKey = key => 
-    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase();
 
 
 const changeColors = (colors) => {
     Object.keys(colors).map(key => 
-        html.style.setProperty(transformKey(key), colors[key]) 
-    )
+        html.style.setProperty(transformKey(key), colors[key])
+    );
+}
+
+const checkbox = document.querySelector('input[name=theme]');
+
+const checkboxColorMode = JSON.parse(localStorage.getItem('color-mode'));
+
+if (checkboxColorMode) {
+  checkbox.checked = checkboxColorMode
+  changeColors(darkMode);
 }
 
 
 checkbox.addEventListener("change", ({target}) => {
-    target.checked ? changeColors(darkMode) : changeColors(initialColors)
+    target.checked ? changeColors(darkMode) : changeColors(initialColors);
+
+    localStorage.setItem('color-mode', target.checked);
 })
